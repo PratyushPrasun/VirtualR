8cdd4cab96a6b1db6b0c17c9994101d8a5cf91a2
Pratyush <138666886+PratyushPrasun@users.noreply.github.com>
2025-04-06 12:39:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router'
+import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route } from 'react-router'
 import App from './App'
 import { RouterProvider } from 'react-router/dom'
 import HeroSection from './Components/HeroSection'
@@ -25,6 +25,8 @@ import Pricing from './Components/Pricing'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
   </StrictMode>,
 )

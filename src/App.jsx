4d13bd452c281f360e3b7f983cbf06c8c03c7668
Pratyush <138666886+PratyushPrasun@users.noreply.github.com>
2025-04-06 12:37:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
-import { Outlet } from 'react-router'
+import { Outlet, Routes, Route } from 'react-router'
+import HeroSection from './Components/HeroSection'
+import Feature from './Components/Feature'
 
 
 const App = () => {
   return (
     <>
       <Navbar/>
-      <Outlet/>
+      <Routes>
+        <Route path='/' element={<HeroSection/>}/>
+        <Route path='/feature' element={<Feature/>}/>
+      </Routes>
+      <Footer/>
       <Footer/>
     </>
   )
